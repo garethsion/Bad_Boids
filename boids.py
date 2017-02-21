7214fdaf93a16ff5363888c91614e40eefe3e57a
Gareth Jones <garethsion@googlemail.com>
2017-02-21 21:26:38 +0000

    Started to implement pos and vel as numpy array

--- boids.py
+++ boids.py
@@ -6,16 +6,17 @@ for use as an exercise on refactoring.
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
+from numpy import array
 
 # Deliberately terrible code for teaching purposes
 
-range_int = 50 # removed unnecessary repetition of this number
+number_of_boids = 50 # removed unnecessary repetition of this number
 
-x_pos_bounds = [-450, 50.0]
-y_pos_bounds = [300.0, 600.0]
+x_pos_bounds = array([-450, 50.0])
+y_pos_bounds = array([300.0, 600.0])
 
-x_vel_bounds = [0, 10.0]
-y_vel_bounds = [-20.0, 20.0]
+x_vel_bounds = array([0, 10.0])
+y_vel_bounds = array([-20.0, 20.0])
 
 x_axis_limits = [-500,1500]
 y_axis_limits = [-500,1500]
@@ -23,10 +24,10 @@ y_axis_limits = [-500,1500]
 anim_frame_rate = 50
 anim_interval = 50
 
-boids_x=[random.uniform(x_pos_bounds[0],x_pos_bounds[1]) for x in range(range_int)]
-boids_y=[random.uniform(y_pos_bounds[0],y_pos_bounds[1]) for x in range(range_int)]
-boid_x_velocities=[random.uniform(x_vel_bounds[0],x_vel_bounds[1]) for x in range(range_int)]
-boid_y_velocities=[random.uniform(y_vel_bounds[0], y_vel_bounds[1]) for x in range(range_int)]
+boids_x=[random.uniform(x_pos_bounds[0],x_pos_bounds[1]) for x in range(number_of_boids)]
+boids_y=[random.uniform(y_pos_bounds[0],y_pos_bounds[1]) for x in range(number_of_boids)]
+boid_x_velocities=[random.uniform(x_vel_bounds[0],x_vel_bounds[1]) for x in range(number_of_boids)]
+boid_y_velocities=[random.uniform(y_vel_bounds[0], y_vel_bounds[1]) for x in range(number_of_boids)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids):
@@ -40,29 +41,29 @@ def update_boids(boids):
     group_flying_y_weight = 0.125
     
     # Fly towards the middle
-    for i in range(range_int):
-	for j in range(range_int):
-	    xvs[i]=xvs[i]+(xs[j]-xs[i])*fly_to_mid_x_weight/range_int
-    for i in range(range_int):
-	for j in range(range_int):
-	    yvs[i]=yvs[i]+(ys[j]-ys[i])*fly_to_mid_y_weight/range_int
+    for i in range(number_of_boids):
+	for j in range(number_of_boids):
+	    xvs[i]=xvs[i]+(xs[j]-xs[i])*fly_to_mid_x_weight/number_of_boids
+    for i in range(number_of_boids):
+	for j in range(number_of_boids):
+	    yvs[i]=yvs[i]+(ys[j]-ys[i])*fly_to_mid_y_weight/number_of_boids
     
     # Fly away from nearby boids
-    for i in range(range_int):
-	for j in range(range_int):
+    for i in range(number_of_boids):
+	for j in range(number_of_boids):
 	    if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < nearby_boid_dist:
 		xvs[i]=xvs[i]+(xs[i]-xs[j])
 		yvs[i]=yvs[i]+(ys[i]-ys[j])
     
     # Try to match speed with nearby boids
-    for i in range(range_int):
-	for j in range(range_int):
+    for i in range(number_of_boids):
+	for j in range(number_of_boids):
 	    if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < flying_dist:
-		xvs[i]=xvs[i]+(xvs[j]-xvs[i])*group_flying_x_weight/range_int
-		yvs[i]=yvs[i]+(yvs[j]-yvs[i])*group_flying_y_weight/range_int
+		xvs[i]=xvs[i]+(xvs[j]-xvs[i])*group_flying_x_weight/number_of_boids
+		yvs[i]=yvs[i]+(yvs[j]-yvs[i])*group_flying_y_weight/number_of_boids
     
     # Move according to velocities
-    for i in range(range_int):
+    for i in range(number_of_boids):
 	xs[i]=xs[i]+xvs[i]
 	ys[i]=ys[i]+yvs[i]
 
