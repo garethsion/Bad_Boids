36dae594f5397a89ccb96aebf611afad3efaea62
Gareth Jones <garethsion@googlemail.com>
2017-02-21 17:46:30 +0000

    Started to clear up indentation in update_boids

--- boids.py
+++ boids.py
@@ -30,39 +30,38 @@ boid_y_velocities=[random.uniform(y_vel_bounds[0], y_vel_bounds[1]) for x in ran
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 def update_boids(boids):
-	xs,ys,xvs,yvs=boids
+    xs,ys,xvs,yvs=boids
+    
+    fly_to_mid_x_weight = 0.01
+    fly_to_mid_y_weight = 0.01
+    nearby_boid_dist = 100
+    flying_dist = 10000
+    group_flying_x_weight = 0.125
+    group_flying_y_weight = 0.125
 
-        fly_to_mid_x_weight = 0.01
-        fly_to_mid_y_weight = 0.01
-        nearby_boid_dist = 100
-        flying_dist = 10000
-        group_flying_x_weight = 0.125
-        group_flying_y_weight = 0.125
-
-	# Fly towards the middle
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			xvs[i]=xvs[i]+(xs[j]-xs[i])*fly_to_mid_x_weight/len(xs)
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			yvs[i]=yvs[i]+(ys[j]-ys[i])*fly_to_mid_y_weight/len(xs)
-	# Fly away from nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < nearby_boid_dist:
-				xvs[i]=xvs[i]+(xs[i]-xs[j])
-				yvs[i]=yvs[i]+(ys[i]-ys[j])
-	# Try to match speed with nearby boids
-	for i in range(len(xs)):
-		for j in range(len(xs)):
-			if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < flying_dist:
-				xvs[i]=xvs[i]+(xvs[j]-xvs[i])*group_flying_x_weight/len(xs)
-				yvs[i]=yvs[i]+(yvs[j]-yvs[i])*group_flying_y_weight/len(xs)
-	# Move according to velocities
-	for i in range(len(xs)):
-		xs[i]=xs[i]+xvs[i]
-		ys[i]=ys[i]+yvs[i]
-
+    # Fly towards the middle
+    for i in range(len(xs)):
+	    for j in range(len(xs)):
+		    xvs[i]=xvs[i]+(xs[j]-xs[i])*fly_to_mid_x_weight/len(xs)
+    for i in range(len(xs)):
+	    for j in range(len(xs)):
+		    yvs[i]=yvs[i]+(ys[j]-ys[i])*fly_to_mid_y_weight/len(xs)
+    # Fly away from nearby boids
+    for i in range(len(xs)):
+	    for j in range(len(xs)):
+		    if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < nearby_boid_dist:
+			    xvs[i]=xvs[i]+(xs[i]-xs[j])
+			    yvs[i]=yvs[i]+(ys[i]-ys[j])
+    # Try to match speed with nearby boids
+    for i in range(len(xs)):
+	    for j in range(len(xs)):
+		    if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < flying_dist:
+			    xvs[i]=xvs[i]+(xvs[j]-xvs[i])*group_flying_x_weight/len(xs)
+			    yvs[i]=yvs[i]+(yvs[j]-yvs[i])*group_flying_y_weight/len(xs)
+    # Move according to velocities
+    for i in range(len(xs)):
+	    xs[i]=xs[i]+xvs[i]
+	    ys[i]=ys[i]+yvs[i]
 
 figure=plt.figure()
 axes=plt.axes(xlim=(x_axis_limits[0],x_axis_limits[1]), ylim=(y_axis_limits[0],y_axis_limits[1]))
