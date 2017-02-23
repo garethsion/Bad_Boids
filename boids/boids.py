2f5fa41de106a923533c3c5d076932c7982eaa50
Gareth Jones <garethsion@googlemail.com>
2017-02-23 19:19:59 +0000

    Improving class structure

--- boids/boids.py
+++ boids/boids.py
@@ -1,96 +1,112 @@
-from matplotlib import pyplot as plt
-from matplotlib import animation
+###from matplotlib import pyplot as plt
+###from matplotlib import animation
 import random
 from numpy import array
 
-number_of_boids = 50
+###number_of_boids = 50
 
-x_pos_bounds = array([-450, 50.0])
-y_pos_bounds = array([300.0, 600.0])
+###x_pos_bounds = array([-450, 50.0])
+###y_pos_bounds = array([300.0, 600.0])
 
-x_vel_bounds = array([0, 10.0])
-y_vel_bounds = array([-20.0, 20.0])
+###x_vel_bounds = array([0, 10.0])
+###y_vel_bounds = array([-20.0, 20.0])
 
-x_axis_limits = [-500,1500]
-y_axis_limits = [-500,1500]
+###x_axis_limits = [-500,1500]
+###y_axis_limits = [-500,1500]
 
-anim_frame_rate = 50
-anim_interval = 50
+###anim_frame_rate = 50
+###anim_interval = 50
 
-fly_to_mid_weight = 0.01
-nearby_boid_dist = 100
-flying_dist = 10000
-group_flying_weight = 0.125
+###fly_to_mid_weight = 0.01
+###nearby_boid_dist = 100
+###flying_dist = 10000
+###group_flying_weight = 0.125
     
 class Boid(object):
-    def __init__(self, owner, position, velocity):
+    def __init__(self, flock, position, velocity):
         self.position = position
         self.velocity = velocity
-        self.flock = owner
+        self.flock = flock
 		
     def flight(self, target):
-        flight = array([0.0,0.0])
+        #flight = array([0.0,0.0])
 		
-	x_seperation = target.pos[0] - self.pos[0]
-	y_seperation = target.pos[1] - self.pos[1]
-	x_vel_seperation = target.vel[0] - self.vel[0]
-	y_vel_seperation = target.vel[1] - self.vel[1]
-		
+	position_sep = target.position - self.position
+        velocity_sep = target.velocity - self.velocity
+        distance = position_sep.dot(position)
+
 	# Fly towards the middle
-        flight[0]+=x_seperation*self.flock.fly_to_mid_weight/self.flock.number_of_boids
-        flight[1]+=y_seperation*self.flock.fly_to_mid_weight/self.flock.number_of_boids
+        self.velocity+=position_sep*self.flock.fly_to_mid_strength/self.flock.number_of_boids
+        
+        ###flight[0]+=x_seperation*self.flock.fly_to_mid_weight/self.flock.number_of_boids
+        ###flight[1]+=y_seperation*self.flock.fly_to_mid_weight/self.flock.number_of_boids
    
         # Fly away from nearby boids
-        if x_seperation**2 + y_seperation**2 < self.flock.nearby_boid_dist:
-            flight[0]-=x_seperation
-            flight[0]-=y_seperation
+        if distance < self.flock.nearby_boid_dist*self.flock.nearby_boid_dist:
+            self.velocity-=relative_position
+        
+        ###if x_seperation**2 + y_seperation**2 < self.flock.nearby_boid_dist:
+            ###flight[0]-=x_seperation
+            ###flight[0]-=y_seperation
      
         # Try to match speed with nearby boids
-	if x_seperation**2 + x_seperation**2 < self.flock.flying_dist:
-            flight[0]+=x_velocity_seperation*self.flock.group_flying_weight/
-                self.flock.number_of_boids
-	    flight[1]+=y_velocity_seperation*self.flock.group_flying_weight/
-                self.flock.number_of_boids
+        if distance < self.flock.group_flying_dist*self.flock.group_flying_dist:
+            self.velocity+=velocity_sep*self.flock.group_flying_weight/self.flock.number_of_boids
+        
+        ###if x_seperation**2 + x_seperation**2 < self.flock.flying_dist:
+            ###flight[0]+=x_velocity_seperation*self.flock.group_flying_weight/
+                ###self.flock.number_of_boids
+	    ###flight[1]+=y_velocity_seperation*self.flock.group_flying_weight/
+                ###self.flock.number_of_boids
 		
 	return flight 
 
 class Flock(object):
-    def __init__(self,number_of_boids,flying_dist,group_flying_weight,nearby_boid_dist,
-        fly_to_mid_weight:
+    def __init__(self,
+            number_of_boids = 50,
+            flying_dist = 10000, 
+            group_flying_weight = 0.125,
+            nearby_boid_dist = 10,
+            fly_to_mid_weight 0.01,
+            initial_position = [[-450,50],[300,600]],
+            initial_velocity = [[0,10],[-20,20]]):
+
+        ###self.number_of_boids = number_of_boids
+        ###self.flying_dist = flying_dist
+	###self.group_flying_weight = flying_weight
+	###self.nearby_boid_dist = nearby_boid_dist
+	###self.fly_to_mid_weight = fly_to_mid_weight
 		
-        self.number_of_boids = number_of_boids
-        self.flying_dist = flying_dist
-	self.group_flying_weight = flying_weight
-	self.nearby_boid_dist = nearby_boid_dist
-	self.fly_to_mid_weight = fly_to_mid_weight
-		
-	self.boids = [Boid(array([random.uniform(x_pos_bounds[0], x_pos_bounds[1]),
-	    random.uniform(y_pos_bound[0], y_pos_bounds[1])]), 
-	    array([random.uniform(x_vel_bounds[0], x_vel_bounds[1]), 
-	    random.uniform(y_vel_bounds[0], y_vel_bounds[1])])) 
+	self.boids = [Boid(array([random.uniform(initial_position[0][0], initial_position[0][1]),
+	    random.uniform(initial_position[1][0], initial_position[1][1])]), 
+	    array([random.uniform(initial_velocity[0][0], initial_velocity[0][1]), 
+	    random.uniform(initial_velocity[1][0], initial_velocity[1][1])])) 
 	    for boids in range(number_of_boids)]
 		
     def update_boids(self):
 	for boid in self.boids:
-            flight = array([0.0,0.0])
+            #flight = array([0.0,0.0])
 	    for target in self.boids:
-		flight+=boid.flight(target)
-		boid.velocity+=boid.velocity # right way around?
-		boid.position+=flight
+		boid.flight(target)
+                #flight+=boid.flight(target)
+		#boid.velocity+=boid.velocity # right way around?
+		#boid.position+=flight
+        for boid in self.boids:
+            boid.position+=boid.velocity
 
-flock = Flock(50)
+###flock = Flock(50)
 
-figure=plt.figure()
-axes=plt.axes(xlim=(x_axis_limits[0],x_axis_limits[1]), ylim=(y_axis_limits[0],y_axis_limits[1]))
-scatter=axes.scatter(boids[0],boids[1])
+###figure=plt.figure()
+###axes=plt.axes(xlim=(x_axis_limits[0],x_axis_limits[1]), ylim=(y_axis_limits[0],y_axis_limits[1]))
+###scatter=axes.scatter(boids[0],boids[1])
 
-def animate(frame):
-   update_boids(boids)
-   scatter.set_offsets(zip(boids[0],boids[1]))
+###def animate(frame):
+   ###update_boids(boids)
+   ###scatter.set_offsets(zip(boids[0],boids[1]))
 
 
-anim = animation.FuncAnimation(figure, animate,
-                               frames=anim_frame_rate, interval=anim_interval)
+###anim = animation.FuncAnimation(figure, animate,
+                               ###frames=anim_frame_rate, interval=anim_interval)
 
-if __name__ == "__main__":
-    plt.show()
+###if __name__ == "__main__":
+    ###plt.show()
