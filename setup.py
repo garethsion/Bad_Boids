5eabe9542d8ed5b89f6a3d2835fa249f90610b8f
Gareth Jones <garethsion@googlemail.com>
2017-02-24 01:43:17 +0000

    Modified setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-        name = "boids",
+        name = "good_boids",
         version = "1.0.0",
         author = 'Gareth Jones',
         author_email = 'gareth.jones.16@ucl.ac.uk',
         license = 'The MIT License (MIT)',
         packages = find_packages(exclude=['*test']),
-        scripts = ['scripts/boids'],
+        scripts = ['scripts/good_boids'],
         install_requires = ['argparse', 'numpy', 'PyYaml', 'matplotlib']
 )
